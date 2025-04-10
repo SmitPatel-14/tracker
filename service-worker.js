f8d57787cd0dd82a01a3af3fd4759eec476fb826
SmitPatel-14 <sp3896508@gmail.com>
2025-04-10 13:59:10 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,42 @@
+const CACHE_NAME = 'focus-tracker-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Install service worker and cache necessary files
 self.addEventListener('install', (event) => {
-  console.log('[Service Worker] Install');
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
+// Activate and remove old caches
 self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activate');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
 
+// Fetch files from cache or network
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
