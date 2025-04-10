0a4c836cd8148ca2de72614b998f27d535335990
SmitPatel-14 <sp3896508@gmail.com>
2025-04-10 14:15:19 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,12 +3,12 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
+  '/script.js',
   '/manifest.json',
   '/icons/icon-192.png',
   '/icons/icon-512.png'
 ];
 
-// Install service worker and cache necessary files
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -17,7 +17,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Activate and remove old caches
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -32,7 +31,6 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Fetch files from cache or network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
